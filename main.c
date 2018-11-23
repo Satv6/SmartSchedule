5df2802b62975d251591e21cd699b0967b0086c7
tonybebe <45278888+tonybebe@users.noreply.github.com>
2018-11-22 22:36:46 -0600

    cambio(tony)
    evento de cerrar con el tache

--- main.c
+++ main.c
@@ -1,38 +1,47 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <SDL2/SDL.h>
-
-//Dimensiones constantes de la ventana
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-
-int main(int argc, char *argv[])
-{
-    SDL_Window *ventana = NULL;
-    SDL_Surface *pantalla = NULL;
-    SDL_Event evento;
-
-    if(SDL_Init(SDL_INIT_VIDEO) < 0){
-        fprintf(stderr, "No se ha podido inicializar SDL de forma correcta: %s\n", SDL_GetError());
-        exit(1);
-    }
-    else{
-        ventana = SDL_CreateWindow("SmartSchedule", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-        if(ventana == NULL){
-            printf("La ventana no ha podido ser creada. Error: %s\n", SDL_GetError());
-        }
-        else{
-            pantalla = SDL_GetWindowSurface(ventana);
-
-            //Rellenar la pantalla de color banco
-            SDL_FillRect(pantalla, NULL, SDL_MapRGB(pantalla->format, 0xFF, 0xFF, 0xFF));
-            //Actualizar la ventana
-            SDL_UpdateWindowSurface(ventana);
-            SDL_Delay(3000);
-        }
-    }
-
-    SDL_DestroyWindow(ventana);
-    SDL_Quit();
-    return 0;
+#include<stdio.h>
+#include<SDL2/SDL.h>
+#include <stdbool.h>
+void botonX();
+SDL_Window * window = NULL; //ventana que se va a mostrar
+SDL_Surface * screenSurface = NULL; //superficie donde se va a mostrar
+void botonX(){//hacer que el programa cierre hasta que le den al boton X
+			bool quit = false;
+			SDL_Event e;
+			while( !quit )
+			{
+				while( SDL_PollEvent( &e ) != 0 )
+				{
+					if( e.type == SDL_QUIT )
+					{
+						quit = true;
+					}
+				}
+            }
 }
+
+int main (int argc, char * args []) {
+
+     if(SDL_Init(SDL_INIT_VIDEO)<0){
+        printf("Error no se pudo iniciar SDL SDL_ERROR: %c",SDL_GetError());
+     }
+       else {
+        window = SDL_CreateWindow ("Agenda", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+       if (window == NULL) {
+            printf ("No se pudo crear la ventana SDL_Error:% s \ n", SDL_GetError ());
+       }
+
+       else {
+            screenSurface = SDL_GetWindowSurface (window); //obtener superficie
+            SDL_FillRect (screenSurface, NULL, SDL_MapRGB (screenSurface-> format, 0x33,0x66,0xFF)); //rellena la superficie
+            SDL_UpdateWindowSurface(window);
+            botonX();
+
+            }
+        }
+    SDL_DestroyWindow (window);
+    SDL_Quit ();
+    return 0;
+     }
+
+
+
